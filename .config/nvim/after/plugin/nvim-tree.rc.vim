228c875869f51dc62341a49ad46e9edf08af519c
Chuanqi <chuanqi.sun.dev@gmail.com>
2022-12-21 20:27:07 -0800

    reset

--- .config/nvim/after/plugin/nvim-tree.rc.vim
+++
@@ -1,5 +0,0 @@
-nnoremap <C-n> :NvimTreeToggle<CR>
-
-lua << EOF
-require('nvim-tree').setup()
-EOF
--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 # neovim
 mkdir -p "$HOME/.config/nvim"
-ln -sf "$HOME/dotfiles/.config/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+ln -sf "$(pwd)/nvim/init.vim" "$HOME/.config/nvim/init.vim"
