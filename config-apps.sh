74a2682cc9ddc0a5101041738e965c50e8dc72da
Chuanqi <chuanqi.sun.dev@gmail.com>
2022-12-22 00:14:52 -0800

    tmux

--- config-apps.sh
+++ config-apps.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+export XDG_CONFIG_HOME="$HOME/.config"
 
 # neovim
-mkdir -p "$HOME/.config/nvim"
-ln -sf "$(pwd)/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+mkdir -p "$XDG_CONFIG_HOME/nvim"
+ln -sf "$(pwd)/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
+
+mkdir -p "$XDG_CONFIG_HOME/tmux"
+ln -sf "$(pwd)/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
