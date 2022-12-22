dc059107d9b936986d53aca2c09209d9aa915d80
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2022-12-22 01:39:00 -0800

    add zsh

--- config-apps.sh
+++ config-apps.sh
@@ -5,5 +5,9 @@ export XDG_CONFIG_HOME="$HOME/.config"
 mkdir -p "$XDG_CONFIG_HOME/nvim"
 ln -sf "$(pwd)/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
 
+# tmux
 mkdir -p "$XDG_CONFIG_HOME/tmux"
 ln -sf "$(pwd)/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
+
+# zsh
+ln -sf "$(pwd)/zsh/.zshrc" "$XDG_CONFIG_HOME/.zshrc"
