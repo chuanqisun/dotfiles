1e8f83365918bfa5a37f29a6278f7812d2f5e2bf
Chuanqi <chuanqi.sun.dev@gmail.com>
2022-12-22 23:34:34 -0800

    adjust prettier

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,6 +53,7 @@ require('packer').startup(function(use)
 
   -- Deps
   use "nvim-lua/plenary.nvim" -- required by prettier
+  use "jose-elias-alvarez/null-ls.nvim" -- required by prettier
 
   -- external languages
   use {
@@ -91,62 +92,8 @@ require('packer').startup(function(use)
   }
 
   -- Prettier
-  use 'jose-elias-alvarez/null-ls.nvim'
   use 'MunifTanjim/prettier.nvim'
-
-  local null_ls = require("null-ls")
-
-  local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-  local event = "BufWritePre" -- or "BufWritePost"
-  local async = event == "BufWritePost"
-
-  null_ls.setup({
-    on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        vim.keymap.set("n", "<C-k>f", function()
-          vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-        end, { buffer = bufnr, desc = "[lsp] format" })
-
-        -- format on save
-        vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-        vim.api.nvim_create_autocmd(event, {
-          buffer = bufnr,
-          group = group,
-          callback = function()
-            vim.lsp.buf.format({ bufnr = bufnr, async = async })
-          end,
-          desc = "[lsp] format on save",
-        })
-      end
-
-      if client.supports_method("textDocument/rangeFormatting") then
-        vim.keymap.set("x", "<C-k>f", function()
-          vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-        end, { buffer = bufnr, desc = "[lsp] format" })
-      end
-    end,
-  })
-
-  local prettier = require("prettier")
-
-  prettier.setup({
-    bin = 'prettier',
-    cli_options = {
-      print_width = 160,
-      tab_width = 2,
-      use_tabs = false,
-    },
-    filetypes = {
-      "css",
-      "html",
-      "javascript",
-      "javascriptreact",
-      "json",
-      "typescript",
-      "typescriptreact",
-      "yaml",
-      "toml",
-    },
-  })
+  require("prettier").setup({bin = 'prettierd'})
+  vim.keymap.set('n', '<leader>kf', ':Prettier<CR>', { noremap = true})
 
 end)
