f8b6f2c34a8447e397b136321652f827d32b3b31
Chuanqi <chuanqi.sun.dev@gmail.com>
2022-12-27 15:06:47 -0800

    add tmux legacy compat

--- config-apps.sh
+++ config-apps.sh
@@ -8,6 +8,7 @@ ln -sf "$(pwd)/nvim/init.lua" "$XDG_CONFIG_HOME/nvim/init.lua"
 # tmux
 mkdir -p "$XDG_CONFIG_HOME/tmux"
 ln -sf "$(pwd)/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
+ln -sf "$(pwd)/tmux/tmux.conf" "$HOME/.tmux.conf" # for tmux vesion < 3.1
 
 # zsh
 ln -sf "$(pwd)/zsh/.zshrc" "$HOME/.zshrc"
